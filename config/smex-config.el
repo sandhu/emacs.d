521305ad604232dc46d64851d56aa91362e82816
Achint Sandhu <sandhu@teppoudo.org>
2013-08-20 09:12:49 -0400

    Cleaned up smex support

--- config/smex-config.el
+++ config/smex-config.el
@@ -1,6 +1,9 @@
 (require-package 'smex)
 
-(setq smex-save-file (concat user-emacs-directory ".smex-items"))
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(after 'smex-autoloads
+  (setq smex-save-file (concat user-emacs-directory ".smex-items"))
+  (smex-initialize)
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands))
+
+(provide 'smex-config)
