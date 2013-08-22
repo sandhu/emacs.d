c371fbacaa64f690e375c2debb00d6d777b3c13e
Achint Sandhu <sandhu@teppoudo.org>
2013-08-22 06:39:48 -0400

    Updated how major modes are handled with diminish

--- config/diminish-setup.el
+++ config/diminish-setup.el
@@ -1,5 +1,9 @@
 (require-package 'diminish)
 
+(defmacro diminish-major-mode (mode new-name)
+  `(add-hook (intern (concat (symbol-name ,mode) "-hook"))
+             '(lambda () (setq mode-name ,new-name))))
+
 (after 'diminish-autoloads
   (diminish 'auto-fill-function "")
   (after 'auto-indent-mode (diminish 'auto-indent-minor-mode ""))
@@ -10,26 +14,24 @@
   (after 'paredit (diminish 'paredit-mode " ()"))
   (after 'auto-complete (diminish 'auto-complete-mode  " α"))
 
-  (add-hook 'lisp-mode-hook (lambda () (setq mode-name "λ")))
-  (add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "ξλ")))
-  (add-hook 'lisp-interaction-mode-hook (lambda () (setq mode-name "λ»")))
-  (add-hook 'slime-repl-mode-hook (lambda () (setq mode-name "π»")))
+  (after 'undo-tree
+    (diminish 'undo-tree-mode "")
+    (diminish-major-mode 'undo-tree-visualizer-mode "⅄"))
+
+  (diminish-major-mode 'lisp-mode "λ")
+  (diminish-major-mode 'emacs-lisp-mode "ξλ")
+  (diminish-major-mode 'lisp-interaction-mode "λ»")
+  (diminish-major-mode 'slime-repl-mode "π»")
   (after 'elisp-slime-nav (diminish 'elisp-slime-nav-mode " π"))
   (after 'slime '(diminish 'slime-mode " π")) ; σ
   (after 'eldoc (diminish 'eldoc-mode ""))
 
-  (add-hook 'clojure-mode-hook (lambda () (setq mode-name "Cλ")))
+  (diminish-major-mode 'clojure-mode "Cλ")
   (after 'clojure-test-mode (diminish 'clojure-test-mode ""))
   (after 'nrepl (diminish 'nrepl-interaction-mode " η"))
   (after 'midje-mode (diminish 'midje-mode " Ɱ"))
-  ;;  (add-hook 'hs-minor-mode-hook (diminish 'hs-minor-mode ""))
+
   (after 'kibit-mode (diminish 'kibit-mode " κ"))
 
   (after 'git-gutter-mode (diminish 'git-gutter-mode ""))
   (after 'git-gutter-fringe (diminish 'git-gutter-mode "")))
-
-;;(after 'diminish
-;;(diminish 'undo-tree-mode)
-;;  (diminish 'undo-tree-mode "")
-;;  (add-hook 'undo-tree-visualizer-mode-hook
-;;            (lambda () (setq mode-name "⅄"))))
