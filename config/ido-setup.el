917b392f6fe57ff227e1cc4908d92f0e9b296fe2
Achint Sandhu <sandhu@teppoudo.org>
2013-11-21 08:37:32 -0500

    Added support for the flx matching engine

--- config/ido-setup.el
+++ config/ido-setup.el
@@ -1,5 +1,6 @@
 (require-package 'ido-ubiquitous)
 (require-package 'idomenu)
+(require-package 'flx-ido)
 
 (after 'ido-ubiquitous-autoloads
   (ido-mode t)
@@ -12,6 +13,11 @@
         ido-use-virtual-buffers t
         ido-handle-duplicate-virtual-buffers 2
         ido-max-prospects 10)
+
+  (after 'flx-ido-autoloads
+    (flx-ido-mode t)
+    ;; disable ido faces to see flx highlights.
+    (setq ido-use-faces nil))
 
   ;; Make ~ get you home. Fast. Bad idea if the file name is prefixed with ~
   (add-hook 'ido-setup-hook
