2a84cd64199f484171cb8de161234484845fb5a6
Achint Sandhu <sandhu@teppoudo.org>
2014-01-12 13:12:11 -0500

    Going back to auto-indent defaults

--- config/auto-indent-setup.el
+++ config/auto-indent-setup.el
@@ -1,9 +1,4 @@
 (require-package 'auto-indent-mode)
 
 (after 'auto-indent-mode-autoloads
-  (setq auto-indent-mode-untabify-on-yank-or-paste t)
-  (setq auto-indent-delete-trailing-whitespace-on-save-file t)
-  (setq auto-indent-untabify-on-save-file t)
-  (setq auto-indent-on-save-file t)
-  ;;  (setq auto-indent-indent-style 'conservative)
   (auto-indent-global-mode))
