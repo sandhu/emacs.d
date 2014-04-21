fa9e5f3cc0666d96da5bd98ab8319e491adce20d
Achint Sandhu <sandhu@teppoudo.org>
2014-04-21 15:59:14 -0400

    Remove eldoc mode
    eldoc mode with cider was causing the repl to hang

--- config/clojure-setup.el
+++ config/clojure-setup.el
@@ -89,7 +89,6 @@
                   (clojure-mode))
               (setq mode-name "» Cλ")))
 
-  (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
   (add-hook 'cider-connected-hook 'cider-enable-on-existing-clojure-buffers))
 
 ;;
