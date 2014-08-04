08af3e1f75a2a080a8e0d1d49f47c536231996a6
Achint Sandhu <sandhu@teppoudo.org>
2014-08-04 13:36:08 -0400

    Edit XML files in web-mode

--- config/web-setup.el
+++ config/web-setup.el
@@ -1,6 +1,7 @@
 (require-package 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.xml?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
 
 (add-hook 'web-mode-hook
