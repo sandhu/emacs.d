f82a17629ff6cccc59a96dba0830dbbab9e0c001
Achint Sandhu <sandhu@teppoudo.org>
2014-08-10 08:14:52 -0400

    Invert navigation direction on company popups as needed
    At the bottom of a window, the popup appears above. The navigation
    direction needs to be inverted to allow for selection

--- config/company-setup.el
+++ config/company-setup.el
@@ -4,4 +4,8 @@
 (setq company-tooltip-limit 10)
 (setq company-minimum-prefix-length 2)
 
+;; invert the navigation direction if the the completion popup-isearch-match
+;; is displayed on top (happens near the bottom of windows)
+(setq company-tooltip-flip-when-above t)
+
 (global-company-mode 1)
