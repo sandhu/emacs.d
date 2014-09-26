3137b3808f473c93d50ce5c348fdc4a7043888c5
Matthew Darling <matthew.darling@cenx.com>
2014-09-26 11:26:43 -0400

    Re-enable kibit and flymake

--- config/flymake-setup.el
+++ config/flymake-setup.el
@@ -1,20 +1,20 @@
-;; (require-package 'flymake-cursor)
+(require-package 'flymake-cursor)
 
-;; ;; turn off GUI warnings from flymake because they (calls to message-box)
-;; ;; cause Emacs to hang on OS X
-;; (setq flymake-gui-warnings-enabled nil)
+;; turn off GUI warnings from flymake because they (calls to message-box)
+;; cause Emacs to hang on OS X
+(setq flymake-gui-warnings-enabled nil)
 
-;; ;; alias the new `flymake-report-status-slim' to `flymake-report-status'
-;; (defun flymake-report-status-slim (e-w &optional status)
-;;   "Show \"slim\" flymake status in mode line."
-;;   (when e-w
-;;     (setq flymake-mode-line-e-w e-w))
-;;   (when status
-;;     `        (setq flymake-mode-line-status status))
-;;   (let* ((mode-line " Φ"))
-;;     (when (> (length flymake-mode-line-e-w) 0)
-;;       (setq mode-line (concat mode-line ":" flymake-mode-line-e-w)))
-;;     (setq mode-line (concat mode-line flymake-mode-line-status))
-;;     (setq flymake-mode-line mode-line)
-;;     (force-mode-line-update)))
-;; (defalias 'flymake-report-status 'flymake-report-status-slim)
+;; alias the new `flymake-report-status-slim' to `flymake-report-status'
+(defun flymake-report-status-slim (e-w &optional status)
+  "Show \"slim\" flymake status in mode line."
+  (when e-w
+    (setq flymake-mode-line-e-w e-w))
+  (when status
+    `        (setq flymake-mode-line-status status))
+  (let* ((mode-line " Φ"))
+    (when (> (length flymake-mode-line-e-w) 0)
+      (setq mode-line (concat mode-line ":" flymake-mode-line-e-w)))
+    (setq mode-line (concat mode-line flymake-mode-line-status))
+    (setq flymake-mode-line mode-line)
+    (force-mode-line-update)))
+(defalias 'flymake-report-status 'flymake-report-status-slim)
