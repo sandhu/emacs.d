ef9acc6b4331781456268b3f5fc6e8538a1cd3ac
Achint Sandhu <sandhu@teppoudo.org>
2015-04-20 10:27:55 -0400

    Upgrade dependencies in profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -12,31 +12,31 @@
             [lein-test-out "0.3.1"]
 
             ;; Package management
-            [lein-ancient "0.6.3"]
+            [lein-ancient "0.6.7"]
             [lein-licenses "0.2.0"]
             [lein-clojars "0.9.1"]
 
             ;; Documentation
-            [codox "0.8.10"]
+            [codox "0.8.11"]
             [lein-clojuredocs "1.0.2"]
 
             ;; Static analysis
             [lein-typed "0.3.5"]
             ;; [jonase/eastwood "0.1.2"]
             [lein-bikeshed "0.2.0"]
-            [lein-kibit "0.0.8"]]
+            [lein-kibit "0.1.2"]]
 
-  :dependencies [[org.clojure/tools.nrepl "0.2.7"]
+  :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                  [org.clojars.gjahad/debug-repl "0.3.3"]
                  [difform "1.1.2"]
 
                  [spyscope "0.1.5"]
                  [org.clojure/tools.trace "0.7.8"]
-                 [org.clojure/tools.namespace "0.2.9"]
+                 [org.clojure/tools.namespace "0.2.10"]
                  [alembic "0.3.2"]
                  [im.chit/vinyasa.inject "0.3.4"]
                  [im.chit/vinyasa.reflection "0.3.4"]
-                 [io.aviso/pretty "0.1.8"]
+                 [io.aviso/pretty "0.1.17"]
 
                  [slamhound "1.5.5"]
                  [criterium "0.4.3"]]
