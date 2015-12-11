856d525b5ee7c26f0485ec9c372862f028184bef
Achint Sandhu <sandhu@7theta.com>
2015-12-11 08:49:15 -0500

    Upgrade to cider 0.10.0

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,7 @@
 {:user
  {:jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
-  :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+  :plugins [[cider/cider-nrepl "0.10.0"]
             [refactor-nrepl "2.0.0-SNAPSHOT"]
-
-            [lein-ancient "0.6.7"]]
-
+            [lein-ancient "0.6.8"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                  [criterium "0.4.3"]]}}
