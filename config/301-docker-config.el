17e9b44d10344a0eff4080afdfda70492ccc57c1
Achint Sandhu <sandhu@7theta.com>
2016-03-15 09:34:03 -0400

    Disable auto-indent-mode for dockerfile and conf

--- config/301-docker-config.el
+++ config/301-docker-config.el
@@ -2,4 +2,7 @@
 
 (use-package docker :ensure t)
 
-(use-package dockerfile-mode :ensure t)
+(use-package dockerfile-mode :ensure t
+  :init (setq auto-indent-disabled-modes-list
+              (append '(dockerfile-mode conf-unix-mode)
+                      auto-indent-disabled-modes-list)))
