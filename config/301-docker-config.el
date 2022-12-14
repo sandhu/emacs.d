bea58cc659e98a9cfb6f64e8abe26bc05f22ddbf
Achint Sandhu <sandhu@7theta.com>
2022-12-14 07:41:59 -0500

    Disable indentation for dockerfiles

--- config/301-docker-config.el
+++ config/301-docker-config.el
@@ -3,6 +3,6 @@
 (use-package docker :ensure t)
 
 (use-package dockerfile-mode :ensure t
-  :init (setq auto-indent-disabled-modes-list
-              (append '(dockerfile-mode conf-unix-mode)
-                      auto-indent-disabled-modes-list)))
+  :config (add-hook 'dockerfile-mode-hook
+                    (lambda ()
+                      (aggressive-indent-mode 0))))
