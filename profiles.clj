7a592fa5cd6227acfbe72bad552831701fb91208
Achint Sandhu <sandhu@7theta.com>
2023-08-16 09:15:44 -0400

    Add lein vizdeps

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[lein-ancient "1.0.0-RC3"]
-                  [lein-pprint "1.3.2"]]}}
+                  [lein-pprint "1.3.2"]
+                  [org.clj-commons/lein-vizdeps "1.0"]]}}
